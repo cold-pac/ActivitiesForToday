af19af92cdb83ed50dd3d927c872c49fe1576c10
cold-pac <erol.a.cimen@gmail.com>
2020-06-18 00:37:13 +1000

    Updates

---
+++ precache-manifest.d35a52b1d4e7d9d6d6513242d70fd7b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb1d236b61a3ca9ec922a51aba07cfc",
+    "url": "/ActivitiesForToday/index.html"
+  },
+  {
+    "revision": "f3506f75a6a70d2e98b7",
+    "url": "/ActivitiesForToday/static/css/main.8725a0b7.chunk.css"
+  },
+  {
+    "revision": "b5fbd6be4a903dcf48fd",
+    "url": "/ActivitiesForToday/static/js/2.90ef928d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ActivitiesForToday/static/js/2.90ef928d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3506f75a6a70d2e98b7",
+    "url": "/ActivitiesForToday/static/js/main.afba3f22.chunk.js"
+  },
+  {
+    "revision": "db8ee198ab107f6083bd",
+    "url": "/ActivitiesForToday/static/js/runtime-main.5eacb12b.js"
+  }
+]);
